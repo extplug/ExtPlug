dc126dd1b27c458cb4250d5333b19b11c294de68
René Kooi <rene@kooi.me>
2015-05-05 22:11:49 +0200

    room-styles: fix dj booth location

--- src/plugins/RoomStyles.js
+++ src/plugins/RoomStyles.js
@@ -1,21 +1,21 @@
 define('extplug/plugins/room-styles/main', function (require, exports, module) {
 
-  var Plugin = require('extplug/Plugin'),
-    request = require('extplug/util/request'),
-    fnUtils = require('extplug/util/function'),
-    _ = require('underscore'),
-    $ = require('jquery');
+  const Plugin = require('extplug/Plugin');
+  const request = require('extplug/util/request');
+  const Style = require('extplug/util/Style');
+  const _ = require('underscore');
+  const $ = require('jquery');
 
-  module.exports = Plugin.extend({
+  const RoomStyles = Plugin.extend({
     name: 'Room Styles',
 
     init: function (id, ext) {
       this._super(id, ext);
-      fnUtils.bound(this, 'colors');
-      fnUtils.bound(this, 'css');
-      fnUtils.bound(this, 'images');
-      fnUtils.bound(this, 'unload');
-      fnUtils.bound(this, 'reload');
+      this.colors = this.colors.bind(this);
+      this.css    = this.css.bind(this);
+      this.images = this.images.bind(this);
+      this.unload = this.unload.bind(this);
+      this.reload = this.reload.bind(this);
     },
 
     enable: function () {
@@ -93,15 +93,22 @@ define('extplug/plugins/room-styles/main', function (require, exports, module) {
           playbackImg.attr('src', images.playback);
         }
         if (images.booth) {
-          this.$booth = $('<div />').css({
-            'background': 'url(' + images.booth + ') no-repeat center center',
-            'position': 'absolute',
-            'width': '300px',
-            'height': '100px',
-            'left': '15px',
-            'top': '70px',
-            'z-index': -1
-          }).appendTo(this.$('#dj-booth'));
+          this.Style({
+            '.extplug-booth': {
+              'position': 'absolute',
+              'width': '300px',
+              'height': '100px',
+              'left': '15px',
+              'top': '135px',
+              'z-index': -1
+            }
+          });
+          this.$booth = $('<div />')
+            // plug³ compatibility
+            .attr('id', 'p3-dj-booth')
+            .addClass('extplug-booth')
+            .css({ 'background': 'url(' + images.booth + ') no-repeat center center' })
+            .appendTo(this.$('#dj-booth'));
         }
       }
     },
@@ -128,6 +135,8 @@ define('extplug/plugins/room-styles/main', function (require, exports, module) {
     }
 
   });
+
+  module.exports = RoomStyles;
 
 });
 
