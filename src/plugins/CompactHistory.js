482a9744e8e99cdf1680c2976ea493e074710aef
René Kooi <rene@kooi.me>
2015-05-17 21:43:30 +0200

    compact-history: es6ify plugin & reorganise styles

--- src/plugins/CompactHistory.js
+++ src/plugins/CompactHistory.js
@@ -1,64 +1,70 @@
 define('extplug/plugins/compact-history/main', function (require, exports, module) {
 
-  var Plugin = require('extplug/Plugin'),
-    _ = require('underscore'),
-    $ = require('jquery');
+  const Plugin = require('extplug/Plugin');
+  const _ = require('underscore');
+  const $ = require('jquery');
 
-  module.exports = Plugin.extend({
+  const ITEM_HEIGHT = 20;
+
+  const CompactHistory = Plugin.extend({
     name: 'Compact History',
     description: 'Lays out the room history in a much more compact view.',
 
     // We'll just use CSS
-    enable: function () {
+    enable() {
       this._super();
-      var ITEM_HEIGHT = 20;
-      var heightPx = ITEM_HEIGHT + 'px';
       this.Style({
         '#history-panel .media-list.history .playlist-media-item:not(.selected)': {
-          'height': heightPx
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) img': {
-          'height': heightPx,
-          'width': (ITEM_HEIGHT * 1.5) + 'px',
-          'margin-top': '0px'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .score': {
-          'height': 'auto',
-          'width': 'auto',
-          'top': '0px',
-          'left': '65px'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .score .item': {
-          'margin-right': '10px'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .meta': {
-          'height': 'auto'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .meta span': {
-          'height': heightPx,
-          'top': '0px'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .actions': {
-          'height': heightPx,
-          'top': '0px'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .author': {
-          'left': '120px',
-          'right': '300px',
-          'width': 'auto'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .name': {
-          'right': '125px'
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .actions div': {
-          'height': heightPx
-        },
-        '#history-panel .media-list.history .playlist-media-item:not(.selected) .actions div i': {
-          'top': '-4px'
+          'height': `${ITEM_HEIGHT}px`,
+
+          'img': {
+            'height': `${ITEM_HEIGHT}px`,
+            'width': `${ITEM_HEIGHT * 1.5}px`,
+            'margin-top': '0px'
+          },
+
+          '.score': {
+            'height': 'auto',
+            'width': 'auto',
+            'top': '0px',
+            'left': '65px',
+            '.item': {
+              'margin-right': '10px'
+            }
+          },
+
+          '.meta': {
+            'height': 'auto',
+            'span': {
+              'height': `${ITEM_HEIGHT}px`,
+              'top': '0px'
+            },
+            '.author': {
+              'left': '120px',
+              'right': '300px',
+              'width': 'auto'
+            },
+            '.name': {
+              'right': '125px'
+            }
+          },
+
+          '.actions': {
+            'height': `${ITEM_HEIGHT}px`,
+            'top': '0px',
+            'div': {
+              'height': `${ITEM_HEIGHT}px`,
+              'i': {
+                'top': '-4px'
+              }
+            }
+          }
         }
       });
     }
 
   });
+
+  module.exports = CompactHistory;
 
 });
