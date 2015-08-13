905fbfc6fca757f9c46ebbce470662f2ba66dd1b
René Kooi <rene@kooi.me>
2015-08-13 14:09:40 +0200

    user-classes: add user classes to UserView, closes #24

--- src/plugins/UserClassesPlugin.js
+++ src/plugins/UserClassesPlugin.js
@@ -3,10 +3,13 @@ define(function (require, exporst, module) {
   const Plugin = require('../Plugin');
   const getUserClasses = require('../util/getUserClasses');
   const Events = require('plug/core/Events');
+  const currentUser = require('plug/models/currentUser');
+  const UserView = require('plug/views/users/UserView');
   const UserRowView = require('plug/views/rooms/users/RoomUserRowView');
   const WaitListRowView = require('plug/views/rooms/users/WaitListRowView');
   const userRolloverView = require('plug/views/users/userRolloverView');
   const { after } = require('meld');
+  const { defer } = require('underscore');
 
   const r = API.ROLE;
   const { roleClasses } = getUserClasses;
@@ -36,11 +39,19 @@ define(function (require, exporst, module) {
           this.$el.addClass(getUserClasses(id).join(' '));
         }
       });
+      this.userViewClasses = after(UserView.prototype, 'render', function () {
+        // `this` is the user view
+        this.$el.addClass(getUserClasses(API.getUser().id));
+      });
+      this.setUserViewClass();
+      // guest change, mostly
+      this.listenTo(currentUser, 'change:id change:role change:gRole', this.setUserViewClass);
     },
     disable() {
       this.rowClasses.remove();
       this.waitListClasses.remove();
       this.rolloverClasses.remove();
+      this.userViewClasses.remove();
     },
 
     onChat(msg) {
@@ -76,6 +87,15 @@ define(function (require, exporst, module) {
       }
 
       msg.classes = classes.join(' ');
+    },
+
+    setUserViewClass() {
+      defer(() => {
+        $('#user-view')
+          .removeClass()
+          .addClass('app-left')
+          .addClass(getUserClasses(API.getUser().id).join(' '));
+      });
     }
   });
 
