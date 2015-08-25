f6debe4d8348b0eed982562312f877cf7297edc9
René Kooi <rene@kooi.me>
2015-08-25 11:05:57 +0200

    emoji-data: `jscs`

--- src/plugins/EmojiDataPlugin.js
+++ src/plugins/EmojiDataPlugin.js
@@ -1,9 +1,9 @@
 define(function (require, exports, module) {
 
-  const Plugin = require('../Plugin')
-  const Events = require('plug/core/Events')
-  const emoji = require('plug/util/emoji')
-  const { around } = require('meld')
+  const Plugin = require('../Plugin');
+  const Events = require('plug/core/Events');
+  const emoji = require('plug/util/emoji');
+  const { around } = require('meld');
 
   const EmojiDataPlugin = Plugin.extend({
     name: 'Emoji Data',
@@ -11,40 +11,40 @@ define(function (require, exports, module) {
 
     enable() {
       this.advice = around(emoji, 'replacement', joinpoint => {
-        let name = joinpoint.args[2]
+        let name = joinpoint.args[2];
         if (!name) {
           // attempt to find the name in the emoji-data map
-          let id = joinpoint.args[0]
-          let data = emoji.data[id]
-          if (data) name = data[3][0]
+          let id = joinpoint.args[0];
+          let data = emoji.data[id];
+          if (data) name = data[3][0];
         }
-        let html = joinpoint.proceed()
+        let html = joinpoint.proceed();
         if (name) {
           return html.replace(
             ' class="emoji-inner',
             ` data-emoji-name="${name}" class="emoji-inner extplug-emoji-${name}`
-          )
+          );
         }
-        return html
-      })
+        return html;
+      });
 
       this.listenTo(Events, 'chat:afterreceive', (msg, el) => {
         el.find('.gemoji-plug').each(function () {
-          let inner = $(this)
-          let emojiName = inner.attr('class').match(/gemoji-plug-(\S+)/)
+          let inner = $(this);
+          let emojiName = inner.attr('class').match(/gemoji-plug-(\S+)/);
           if (emojiName) {
             inner.attr('data-emoji-name', emojiName[1])
-                 .addClass(`extplug-emoji-${name}`)
+                 .addClass(`extplug-emoji-${name}`);
           }
-        })
-      })
+        });
+      });
     },
 
     disable() {
-      this.advice.remove()
+      this.advice.remove();
     }
-  })
+  });
 
-  module.exports = EmojiDataPlugin
+  module.exports = EmojiDataPlugin;
 
-})
+});
