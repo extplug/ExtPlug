cd2ad2815dcc1a12ca81bc6bfc79db8ba767d875
René Kooi <rene@kooi.me>
2015-09-08 21:52:27 +0200

    userscript: add icon link

--- extensions/userscript/extplug.user.js
+++ extensions/userscript/extplug.user.js
@@ -2,6 +2,7 @@
 // @name        ExtPlug
 // @description <%= description %>
 // @version     <%= version %>
+// @icon        https://extplug.com/icon.png
 // @match       https://plug.dj/*
 // @namespace   https://extplug.github.io/
 // @downloadURL https://extplug.github.io/ExtPlug/extplug.user.js
