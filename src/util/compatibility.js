7eb98bc0861a457b344086202b2120b7dc9db17a
René Kooi <renee@kooi.me>
2016-08-17 00:30:30 +0200

    compatibility: add extplug/store/settings export

--- src/util/compatibility.js
+++ src/util/compatibility.js
@@ -1,11 +1,13 @@
 import * as meld from 'meld';
 import Plugin from '../Plugin';
+import settings from '../store/settings';
 import * as request from './request';
 import getUserClasses from './getUserClasses';
 import Style from './Style';
 
 window.define('meld', () => meld);
 window.define('extplug/Plugin', () => Plugin);
+window.define('extplug/store/settings', () => settings);
 window.define('extplug/util/request', () => request);
 window.define('extplug/util/getUserClasses', () => getUserClasses);
 window.define('extplug/util/Style', () => Style);
