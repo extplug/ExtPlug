97c0e80f1c90d98364f06e12b0a7d232cf4145c0
René Kooi <renee@kooi.me>
2017-02-07 13:42:23 +0100

    Minify CSS in production build.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
 const cssnext = require('postcss-cssnext');
+const cssnano = require('cssnano');
+
+let plugins;
+
+if (process.env.NODE_ENV === 'production') {
+  plugins = [
+    cssnext({
+      features: {
+        autoprefixer: false,
+      },
+    }),
+    cssnano(),
+  ];
+} else {
+  plugins = [cssnext()];
+}
 
 module.exports = {
-  plugins: [
-    cssnext(),
-  ],
+  plugins,
 };
