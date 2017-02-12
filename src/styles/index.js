284063663db9879584f6ddafd8add5dea491c57c
René Kooi <renee@kooi.me>
2017-02-12 13:50:27 +0100

    Make space for Plugins menu item on small screens.

--- src/styles/index.js
+++ src/styles/index.js
@@ -13,3 +13,4 @@ export pluginSearchBar from './PluginSearchBar.css';
 export pluginsView from './PluginsView.css';
 export pluginSettingsView from './PluginSettingsView.css';
 export installedPluginsView from './InstalledPluginsView.css';
+export responsiveUserMenu from './ResponsiveUserMenu.css';
