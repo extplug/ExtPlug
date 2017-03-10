975acc2d2464f7655b60b9ef454db2b831723e2e
René Kooi <renee@kooi.me>
2017-03-10 14:25:40 +0100

    Attach event with bel in UserMenuView.

--- src/views/users/menu/UserMenuView.js
+++ src/views/users/menu/UserMenuView.js
@@ -1,23 +1,20 @@
 import { around } from 'meld';
 import BaseView from 'plug/views/users/menu/UserMenuView';
-import bel from 'bel';
+import html from 'bel';
 
 export default BaseView.extend({
   render() {
     this._super();
-    this.$el.append(bel`
-      <div class="item extplug-plugins" data-value="extplug-plugins">
+
+    this.$el.append(html`
+      <div class="item extplug-plugins" data-value="extplug-plugins"
+           onclick=${this.onPluginsClick.bind(this)}>
         <i class="icon icon-extplug-plugins"></i>
         <span class="label">Plugins</span>
       </div>
     `);
 
-    this.$('.extplug-plugins').on('click', this.onPluginsClick.bind(this));
-  },
-
-  remove() {
-    this.$('.extplug-plugins').off();
-    this._super();
+    return this;
   },
 
   onPluginsClick() {
