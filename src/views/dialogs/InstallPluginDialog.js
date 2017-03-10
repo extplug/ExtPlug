84a0144b49656c6d7d1f36e28abb839f3cd286a4
René Kooi <renee@kooi.me>
2017-03-10 14:23:39 +0100

    Use bel in InstallPluginDialog.

--- src/views/dialogs/InstallPluginDialog.js
+++ src/views/dialogs/InstallPluginDialog.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import { defer } from 'underscore';
+import html from 'bel';
 import Dialog from 'plug/views/dialogs/Dialog';
 import Events from 'plug/core/Events';
 import PluginInstallationEvent from '../../events/PluginInstallationEvent';
@@ -11,30 +12,32 @@ const InstallPluginDialog = Dialog.extend({
   render() {
     // don't overlay chat
     $('#dialog-container').addClass('is-preview');
-    this.$input = $('<input />').attr({
-      type: 'text',
-      placeholder: 'https://',
-    });
-    this.$wrap = $('<div />')
-      .addClass('dialog-input-background')
-      .append(this.$input);
+    this.input = html`
+      <input type="text" placeholder="https://" />
+    `;
+
     this.$el
       .append(this.getHeader('Install Plugin'))
       .append(this.getBody()
         .append(this.getMessage('Enter the URL of the plugin you want to install:'))
-        .append(this.$wrap))
+        .append(html`
+          <div class="dialog-input-background">
+            ${this.input}
+          </div>
+        `))
       .append(this.getButtons('Install', true));
+
     defer(this.deferFocus.bind(this));
+
     return this._super();
   },
 
   deferFocus() {
-    this.$input.focus();
+    this.input.focus();
   },
 
   submit() {
-    const inp = this.$input;
-    const url = inp.val();
+    const url = this.input.value;
     if (url.length > 0 && url.length > 0) {
       Events.dispatch(new PluginInstallationEvent(
         PluginInstallationEvent.INSTALL, { name: '', url }));
@@ -43,7 +46,6 @@ const InstallPluginDialog = Dialog.extend({
 
   close() {
     $('#dialog-container').removeClass('is-preview');
-    this.$input.off();
     this._super();
   },
 });
