84dd7ad2e4d97196f79aad55f0162cede849e915
René Kooi <renee@kooi.me>
2017-03-20 15:37:40 +0100

    deps: gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,24 @@ const gulpif = require('gulp-if');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const del = require('del');
-const merge = require('merge-stream');
 const mkdirp = require('mkdirp');
 const rename = require('gulp-rename');
 const rjs = require('requirejs');
-const runseq = require('run-sequence');
 const template = require('gulp-template');
 const zip = require('gulp-zip');
 const webpack = require('webpack');
-const watch = require('gulp-watch');
 const packg = require('./package.json');
 
-gulp.task('clean-lib', () => del('lib'));
-gulp.task('clean-build', () => del('build'));
-gulp.task('clean', ['clean-lib', 'clean-build']);
+function cleanLib() {
+  return del('lib');
+}
+function cleanBuild() {
+  return del('build');
+}
+const clean = gulp.parallel(
+  cleanLib,
+  cleanBuild
+);
 
 function createWebpackConfig(options) {
   return {
@@ -92,24 +96,24 @@ function createWebpackConfig(options) {
   };
 }
 
-gulp.task('build:source', (done) => {
+function buildSource(done) {
   webpack(createWebpackConfig({
     minify: !!env.minify,
   }), done);
-});
+}
 
-gulp.task('build:loader:transform', () => (
-  gulp.src('src/main.js')
+function buildLoaderTransform() {
+  return gulp.src('src/main.js')
     .pipe(babel({
       presets: [
         ['extplug', { modules: 'amd' }],
       ],
     }))
     .pipe(rename('init.js'))
-    .pipe(gulp.dest('build'))
-));
+    .pipe(gulp.dest('build'));
+}
 
-gulp.task('build:loader', ['build:loader:transform'], (done) => {
+function buildLoaderBundle(done) {
   rjs.optimize({
     baseUrl: './',
     name: 'extplug/loader',
@@ -131,16 +135,18 @@ gulp.task('build:loader', ['build:loader:transform'], (done) => {
       });
     },
   });
-});
+}
 
-gulp.task('concat', ['build:loader', 'build:source'], () => (
-  gulp.src(['build/loader.js', 'build/source.js'])
+const buildLoader = gulp.series(buildLoaderTransform, buildLoaderBundle);
+
+function concatSource() {
+  return gulp.src(['build/loader.js', 'build/source.js'])
     .pipe(concat('extplug.code.js'))
-    .pipe(gulp.dest('build/'))
-));
+    .pipe(gulp.dest('build/'));
+}
 
-gulp.task('build', ['concat'], () => (
-  gulp.src('src/loader.template.js', { buffer: true })
+function wrapBuiltSourceInLoader() {
+  return gulp.src('src/loader.template.js', { buffer: true })
     .pipe(through2.obj((file, enc, cb) => {
       fs.readFile('build/extplug.code.js', 'utf8', (e, code) => {
         if (e) {
@@ -161,83 +167,84 @@ gulp.task('build', ['concat'], () => (
       mangle: { toplevel: true },
     })))
     .pipe(rename('extplug.js'))
-    .pipe(gulp.dest('build/'))
-));
+    .pipe(gulp.dest('build/'));
+}
 
-gulp.task('chrome-unpacked', () => merge(
-  gulp.src(['extensions/chrome/main.js', 'extensions/chrome/manifest.json'])
+const build = gulp.series(
+  gulp.parallel(buildSource, buildLoader),
+  concatSource,
+  wrapBuiltSourceInLoader
+);
+
+const buildChromeExtensionFiles = gulp.parallel(
+  () => gulp.src(['extensions/chrome/main.js', 'extensions/chrome/manifest.json'])
     .pipe(template(packg))
     .pipe(gulp.dest('build/chrome/')),
 
-  gulp.src(['img/icon*.png'])
+  () => gulp.src(['img/icon*.png'])
     .pipe(gulp.dest('build/chrome/img/')),
 
-  gulp.src(['build/extplug.js'])
+  () => gulp.src(['build/extplug.js'])
     .pipe(concat('extplug.js'))
     .pipe(gulp.dest('build/chrome/'))
-));
+);
 
-gulp.task('chrome-pack', () => (
-  gulp.src('build/chrome/*')
+function packChromeExtension() {
+  return gulp.src('build/chrome/*')
     .pipe(zip('extplug.chrome.zip'))
-    .pipe(gulp.dest('build/'))
-));
+    .pipe(gulp.dest('build/'));
+}
 
-gulp.task('chrome', (cb) => {
-  runseq('chrome-unpacked', 'chrome-pack', cb);
-});
+const buildChromeExtension = gulp.series(buildChromeExtensionFiles, packChromeExtension);
 
-gulp.task('firefox', () => merge(
-  gulp.src(['extensions/firefox/*'])
+const buildFirefoxExtension = gulp.parallel(
+  () => gulp.src(['extensions/firefox/*'])
     .pipe(template(packg))
     .pipe(gulp.dest('build/firefox/')),
 
-  gulp.src(['build/extplug.js'])
+  () => gulp.src(['build/extplug.js'])
     .pipe(gulp.dest('build/firefox/data/'))
-));
+);
 
-gulp.task('userscript-meta', () => (
-  gulp.src(['extensions/userscript/extplug.user.js'])
+function userscriptMeta() {
+  return gulp.src(['extensions/userscript/extplug.user.js'])
     .pipe(template(packg))
     .pipe(rename('extplug.meta.user.js'))
-    .pipe(gulp.dest('build/'))
-));
+    .pipe(gulp.dest('build/'));
+}
 
-gulp.task('userscript', ['userscript-meta'], () => (
-  gulp.src(['build/extplug.meta.user.js', 'build/extplug.js'])
+function userscriptConcat() {
+  return gulp.src(['build/extplug.meta.user.js', 'build/extplug.js'])
     .pipe(concat('extplug.user.js'))
-    .pipe(gulp.dest('build/'))
-));
+    .pipe(gulp.dest('build/'));
+}
 
-gulp.task('watch:chrome', ['default'], () =>
-  watch('src/**/*', () => {
-    runseq('build', 'chrome');
-  })
+const buildUserscript = gulp.series(
+  userscriptMeta,
+  userscriptConcat
 );
 
-gulp.task('watch:firefox', ['default'], () =>
-  watch('src/**/*', () => {
-    runseq('build', 'firefox');
-  })
+const buildExtensions = gulp.parallel(
+  buildChromeExtension,
+  buildFirefoxExtension,
+  buildUserscript
 );
 
-gulp.task('watch:userscript', ['default'], () =>
-  watch('src/**/*', () => {
-    runseq('build', 'userscript');
-  })
+exports.default = gulp.series(
+  cleanBuild,
+  build,
+  buildExtensions
 );
 
-gulp.task('watch', ['default'], () =>
-  watch('src/**/*', () => {
-    runseq('build', ['chrome', 'firefox', 'userscript']);
-  })
-);
-
-gulp.task('default', (cb) => {
-  runseq(
-    'clean-build',
-    'build',
-    ['chrome', 'firefox', 'userscript'],
-    cb
-  );
-});
+exports.clean = clean;
+exports.loader = buildLoader;
+exports.build = build;
+exports.userscript = buildUserscript;
+exports.watchChrome = () =>
+  gulp.watch('src/**/*', gulp.series(build, buildChromeExtension));
+exports.watchFirefox = () =>
+  gulp.watch('src/**/*', gulp.series(build, buildFirefoxExtension));
+exports.watchUserscript = () =>
+  gulp.watch('src/**/*', gulp.series(build, buildUserscript));
+exports.watch = () =>
+  gulp.watch('src/**/*', gulp.series(build, buildExtensions));
